5f93b8cc78ad43b07a4f832480857ee9047ff0c3
Luis-F-Felix-M-CAPS <luis.felix@capstechnology.co>
2026-01-15 16:09:10 -0600

    chore: setup project structure and Entity Framework Core

---
+++ CommentSentimentApi/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CommentSentimentApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommentSentimentApi.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Comment> Comments => Set<Comment>();
+    }
+}
