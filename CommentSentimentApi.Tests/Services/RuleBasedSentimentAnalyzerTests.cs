d2fe77f637f3ca330189475bd540bfb95e8ff849
Luis-F-Felix-M-CAPS <luis.felix@capstechnology.co>
2026-01-15 20:58:40 -0600

    Unit tests were implemented

---
+++ CommentSentimentApi.Tests/Services/RuleBasedSentimentAnalyzerTests.cs
@@ -0,0 +1,41 @@
+﻿using CommentSentimentApi.Application.Services;
+using Xunit;
+
+namespace CommentSentimentApi.Tests.Services
+{
+    public class RuleBasedSentimentAnalyzerTests
+    {
+        [Fact]
+        public void Analyze_ShouldReturnPositive_WhenTextContainsPositiveWords()
+        {
+            var analyzer = new RuleBasedSentimentAnalyzer();
+            var text = "Este producto es excelente y muy bueno";
+
+            var result = analyzer.Analyze(text);
+
+            Assert.Equal("positivo", result);
+        }
+
+        [Fact]
+        public void Analyze_ShouldReturnNegative_WhenTextContainsNegativeWords()
+        {
+            var analyzer = new RuleBasedSentimentAnalyzer();
+            var text = "Este producto tiene un problema terrible";
+
+            var result = analyzer.Analyze(text);
+
+            Assert.Equal("negativo", result);
+        }
+
+        [Fact]
+        public void Analyze_ShouldReturnNeutral_WhenNoKeywordsAreFound()
+        {
+            var analyzer = new RuleBasedSentimentAnalyzer();
+            var text = "El producto fue entregado ayer";
+
+            var result = analyzer.Analyze(text);
+
+            Assert.Equal("neutral", result);
+        }
+    }
+}
---
+++ CommentSentimentApi.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CommentSentimentApi.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
